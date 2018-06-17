d5eefa21834c6432cb7a7a8c7e5ed047c6434fd0
gongfan99 <gongfan99@hotmail.com>
2018-06-17 14:21:24 -0400

    ...

--- generate_QAM_baseband.h
+++ generate_QAM_baseband.h
@@ -7,71 +7,123 @@
 #include <algorithm>
 #include <string>
 
-std::string generate_QAM_baseband(){
-
-const double PI = 3.141592653589793;
-
-unsigned int QAM = 8;
-
-double rollOff = 0.13;
-double symbolRate = 168000;
-unsigned int totalSymbols = 1680;
-unsigned short samplesPerSymbol = 5;
-constexpr unsigned short rrcTapNum = 32;
-
-double sampleRate = symbolRate * samplesPerSymbol;
-double samplePeriod = 1 / sampleRate;
-
-double rrcImpulseResp[rrcTapNum];
-
-int i, j;
-double specialRollOffPoint = 1 / (4 * rollOff);
-double t;
-for (i = 0; i < rrcTapNum; i++){
-  t = (i - rrcTapNum / 2) * samplePeriod;
-  if (abs(t * symbolRate) < 1e-6){
-    rrcImpulseResp[i] = symbolRate * (1 + rollOff * (4 / PI - 1));
-  } else if (abs(t * symbolRate - specialRollOffPoint) < 1e-6 || abs(t * symbolRate + specialRollOffPoint) < 1e-6){
-    rrcImpulseResp[i] = (symbolRate * rollOff / 1.41421356237) * ((1 + 2 / PI) * sin(PI / (4 * rollOff)) + (1 - 2 / PI) * cos(PI / (4 * rollOff)));
-  } else {
-    rrcImpulseResp[i] = (sin(PI * symbolRate * t * (1 - rollOff)) + 4 * rollOff * symbolRate * t * cos(PI * symbolRate * t * (1 + rollOff))) / (PI * t * (1 - 16 * rollOff * rollOff * symbolRate * symbolRate * t * t));
+void generate_IQ_symbol_QAM_random(std::vector<double>& Isymbol, std::vector<double>& Qsymbol, unsigned int totalSymbols, unsigned int QAM){
+  Isymbol.resize(totalSymbols);
+  Qsymbol.resize(totalSymbols);
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_int_distribution<> dis(0, std::sqrt(QAM) - 1);
+  int i;
+  for (i = 0; i < Isymbol.size(); i++){
+    Isymbol[i] = dis(gen) - std::sqrt(QAM) / 2 + 0.5;
+    Qsymbol[i] = dis(gen) - std::sqrt(QAM) / 2 + 0.5;
   }
 }
 
-unsigned int totalSamples = totalSymbols * samplesPerSymbol;
-std::vector<double> Isample(totalSamples, 0);
-std::vector<double> Qsample(totalSamples, 0);
+void generate_baseband_IQ_waveform(std::vector<double>& Isample, std::vector<double>& Qsample, std::vector<double>& Isymbol, std::vector<double>& Qsymbol, double rollOff, double symbolRate, unsigned int samplesPerSymbol, unsigned int rrcTapNum){
+  constexpr double PI = 3.141592653589793;
+  constexpr double sqrt2 = 1.41421356237;
 
-std::random_device rd;  //Will be used to obtain a seed for the random number engine
-std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
-std::uniform_int_distribution<> dis(-QAM, QAM);
-int symbolIValue, symbolQValue;
-for (i = 0; i < totalSymbols; i++){
-  symbolIValue = dis(gen);
-  symbolQValue = dis(gen);
-  for (j = 0; j < rrcTapNum; j++){
-    Isample[(samplesPerSymbol * i + j) % totalSamples] += symbolIValue * rrcImpulseResp[j];
-    Qsample[(samplesPerSymbol * i + j) % totalSamples] += symbolQValue * rrcImpulseResp[j];
+  unsigned int totalSymbols = Isymbol.size();
+  double sampleRate = symbolRate * samplesPerSymbol;
+  double samplePeriod = 1 / sampleRate;
+
+  std::vector<double> rrcImpulseResp(rrcTapNum, 0);
+
+  int i, j;
+  double specialRollOffPoint = 1 / (4 * rollOff);
+  double t;
+  for (i = 0; i < rrcTapNum; i++){
+    t = (i - rrcTapNum / 2) * samplePeriod;
+    if (abs(t * symbolRate) < 1e-6){
+      rrcImpulseResp[i] = symbolRate * (1 + rollOff * (4 / PI - 1));
+    } else if (abs(t * symbolRate - specialRollOffPoint) < 1e-6 || abs(t * symbolRate + specialRollOffPoint) < 1e-6){
+      rrcImpulseResp[i] = (symbolRate * rollOff / sqrt2) * ((1 + 2 / PI) * sin(PI / (4 * rollOff)) + (1 - 2 / PI) * cos(PI / (4 * rollOff)));
+    } else {
+      rrcImpulseResp[i] = (sin(PI * symbolRate * t * (1 - rollOff)) + 4 * rollOff * symbolRate * t * cos(PI * symbolRate * t * (1 + rollOff))) / (PI * t * (1 - 16 * rollOff * rollOff * symbolRate * symbolRate * t * t));
+    }
+  }
+
+  unsigned int totalSamples = totalSymbols * samplesPerSymbol;
+    Isample.resize(totalSamples, 0);
+    Qsample.resize(totalSamples, 0);
+
+  for (i = 0; i < totalSymbols; i++){
+    for (j = 0; j < rrcTapNum; j++){
+      Isample[(samplesPerSymbol * i + j) % totalSamples] += Isymbol[i] * rrcImpulseResp[j];
+      Qsample[(samplesPerSymbol * i + j) % totalSamples] += Qsymbol[i] * rrcImpulseResp[j];
+    }
+  }
+  for (double temp2 : rrcImpulseResp){
+    //std::cout << temp2 << std::endl;
   }
 }
 
-auto minmaxIsample = std::minmax_element(Isample.begin(), Isample.end());
-auto minmaxQsample = std::minmax_element(Qsample.begin(), Qsample.end());
-double temp1[4] = {abs(*minmaxIsample.first), *minmaxIsample.second, abs(*minmaxQsample.first), *minmaxQsample.second};
-double maxIQsample = *std::max_element(temp1, temp1 + 4);
-std::string interleaveIQ(4 * totalSamples, NULL);
-short int int16Value;
-for (i = 0; i < totalSamples; i++){
-  int16Value = Isample[i] * 23000 / maxIQsample;
-  interleaveIQ[4 * i] = (int16Value >> 8) & 0xFF;
-  interleaveIQ[4 * i + 1] = int16Value & 0xFF;
-  int16Value = Qsample[i] * 23000 / maxIQsample;
-  interleaveIQ[4 * i + 2] = (int16Value >> 8) & 0xFF;
-  interleaveIQ[4 * i + 3] = int16Value & 0xFF;
-}
-for (double temp2 : rrcImpulseResp){
-std::cout << temp2 << std::endl;
+void generate_baseband_IQ_waveform_2carriers(std::vector<double>& Isample, std::vector<double>& Qsample, std::vector<double>& Isymbol_1, std::vector<double>& Qsymbol_1, std::vector<double>& Isymbol_2, std::vector<double>& Qsymbol_2, double rollOff, double symbolRate, unsigned int samplesPerSymbol, unsigned int rrcTapNum, double freqGap){
+  constexpr double PI = 3.141592653589793;
+
+  unsigned int totalSymbols = Isymbol_1.size();
+  unsigned int totalSamples = totalSymbols * samplesPerSymbol;
+  double sampleRate = symbolRate * samplesPerSymbol;
+  double samplePeriod = 1 / sampleRate;
+
+  std::vector<double> Isample_1, Qsample_1;
+  generate_baseband_IQ_waveform(Isample_1, Qsample_1, Isymbol_1, Qsymbol_1, rollOff, symbolRate, samplesPerSymbol, rrcTapNum);
+  std::vector<double> Isample_2, Qsample_2;
+  generate_baseband_IQ_waveform(Isample_2, Qsample_2, Isymbol_2, Qsymbol_2, rollOff, symbolRate, samplesPerSymbol, rrcTapNum);
+
+  Isample.resize(totalSamples, 0);
+  Qsample.resize(totalSamples, 0);
+  double sinWT, cosWT;
+  for (int i = 0; i < totalSamples; i++){
+    sinWT = sin(PI * freqGap * i * samplePeriod);
+    cosWT = cos(PI * freqGap * i * samplePeriod);
+    Isample[i] = -Isample_1[i] * sinWT - Qsample_1[i] * cosWT + Isample_2[i] * sinWT - Qsample_2[i] * cosWT;
+    Qsample[i] = Isample_1[i] * cosWT - Qsample_1[i] * sinWT + Isample_2[i] * cosWT + Qsample_2[i] * sinWT;
+  }
 }
 
-return interleaveIQ;
+void download_IQ_sample_VSG(std::vector<double>& Isample, std::vector<double>& Qsample, double sampleRate, std::string& rsrcName){
+  auto minmaxIsample = std::minmax_element(Isample.begin(), Isample.end());
+  auto minmaxQsample = std::minmax_element(Qsample.begin(), Qsample.end());
+  double temp1[4] = {abs(*minmaxIsample.first), *minmaxIsample.second, abs(*minmaxQsample.first), *minmaxQsample.second};
+  double maxIQsample = *std::max_element(temp1, temp1 + 4);
+
+  unsigned int totalSamples = Isample.size();
+  std::string interleaveIQ(4 * totalSamples, NULL);
+  long int16Value;
+  for (int i = 0; i < totalSamples; i++){
+    int16Value = std::lround(Isample[i] * 23000 / maxIQsample);
+    interleaveIQ[4 * i] = (int16Value >> 8) & 0xFF;
+    interleaveIQ[4 * i + 1] = int16Value & 0xFF;
+    int16Value = std::lround(Qsample[i] * 23000 / maxIQsample);
+    interleaveIQ[4 * i + 2] = (int16Value >> 8) & 0xFF;
+    interleaveIQ[4 * i + 3] = int16Value & 0xFF;
+  }
+
+  ViStatus status;
+  ViUInt32 retCount;
+  ViSession defaultRM, instr;
+  status = viOpenDefaultRM(&defaultRM);
+  status = viOpen(defaultRM, (ViRsrc)rsrcName.data(), VI_NULL, VI_NULL, &instr);
+  
+  std::string cmd;
+  cmd = ":SOURce:RADio:ARB:STATe OFF";
+  status = viWrite(instr, (ViBuf)cmd.data(), (ViUInt32)cmd.size(), (ViPUInt32)&retCount);
+  
+  cmd = std::to_string(interleaveIQ.size());
+  cmd = ":MEMory:DATA \"WFM1:FILE1\",#" + std::to_string(cmd.size()) + cmd + interleaveIQ + "\n";
+  status = viWrite(instr, (ViBuf)cmd.data(), (ViUInt32)cmd.size(), (ViPUInt32)&retCount);
+  
+  cmd = ":SOURce:RADio:ARB:WAVeform \"WFM1:FILE1\"\n";
+  status = viWrite(instr, (ViBuf)cmd.data(), (ViUInt32)cmd.size(), (ViPUInt32)&retCount);
+  
+  cmd = ":SOURce:RADio:ARB:SCLock:RATE " + std::to_string(sampleRate) + "\n";
+  status = viWrite(instr, (ViBuf)cmd.data(), (ViUInt32)cmd.size(), (ViPUInt32)&retCount);
+  
+  cmd = ":SOURce:RADio:ARB:STATe ON";
+  status = viWrite(instr, (ViBuf)cmd.data(), (ViUInt32)cmd.size(), (ViPUInt32)&retCount);
+  
+  viClose(instr);
+  viClose(defaultRM);
 }
--- test/test.cc
+++ test/test.cc
@@ -1,18 +1,46 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+#include <random>
 
 #include "visa.h"
 #include "generate_QAM_baseband.h"
 
-int main() {
-  std::string interleaveIQ(generate_QAM_baseband());
-  std::string cmd = std::to_string(interleaveIQ.size());
-  cmd = ":MEM:DATA \"WFM1:FILE1\", #%d%d" + std::to_string(cmd.size()) + cmd;
-  ViStatus status;
-  ViUInt32 retCount;
-  ViSession vi;
-  status = viWrite(vi, (ViBuf)cmd.data(), (ViUInt32)cmd.size(), (ViPUInt32)&retCount);
-  status = viWrite(vi, (ViBuf)interleaveIQ.data(), (ViUInt32)interleaveIQ.size(), (ViPUInt32)&retCount);
-  status = viWrite(vi, (ViBuf)std::string("\n").data(), 1, (ViPUInt32)&retCount);
+int main(int argc, char* argv[]) {
+  unsigned int QAM = 64;
+  double rollOff = 0.13;
+  double symbolRate = 168000;
+  unsigned int totalSymbols = 16;
+  unsigned int samplesPerSymbol = 2;
+  unsigned int rrcTapNum = 32;
+  double freqGap = 1e6;
+  bool singleCarrier = true;
+  std::string rsrcName = "GPIB::1::INSTR";
+
+  if (argc < 3){
+    std::cout << "Usage: test totalSymbols samplesPerSymbol" << std::endl;
+  } else {
+    totalSymbols = atoi(argv[1]);
+    samplesPerSymbol = atoi(argv[2]);
+  }
+
+  std::vector<double> Isample, Qsample;
+  if (singleCarrier){
+    std::vector<double> Isymbol, Qsymbol;
+    generate_IQ_symbol_QAM_random(Isymbol, Qsymbol, totalSymbols, QAM);
+
+    generate_baseband_IQ_waveform(Isample, Qsample, Isymbol, Qsymbol, rollOff, symbolRate, samplesPerSymbol, rrcTapNum);
+  } else {
+    std::vector<double> Isymbol_1, Qsymbol_1;
+    generate_IQ_symbol_QAM_random(Isymbol_1, Qsymbol_1, totalSymbols, QAM);
+    std::vector<double> Isymbol_2, Qsymbol_2;
+    generate_IQ_symbol_QAM_random(Isymbol_2, Qsymbol_2, totalSymbols, QAM);
+
+    generate_baseband_IQ_waveform_2carriers(Isample, Qsample, Isymbol_1, Qsymbol_1, Isymbol_2, Qsymbol_2, rollOff, symbolRate, samplesPerSymbol, rrcTapNum, freqGap);
+  }
+  
+  double sampleRate = symbolRate * samplesPerSymbol;
+  download_IQ_sample_VSG(Isample, Qsample, sampleRate, rsrcName);
+
   std::cout << "generate_QAM_baseband" << std::endl;
 }
